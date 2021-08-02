366225523a2dc63b542c88ceedbce5a8929bfda4
Melby Arguedas Ordonez <Melby.Arguedas@jalasoft.com>
2021-08-01 22:41:32 -0400

    Added second homework

---
+++ Homework2/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+import datetime
+import socket
+import time
+
+now = datetime.datetime.now()
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+timezone1 = time.timezone
+converted_num = str(timezone1)
+
+print("Current date and time : ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+print("The timezone of your computer is:" + converted_num)
